b26b55297ebf1ab3ef635bc5cf0aadcfc60de361
navaneethan.g <navaneethan.g@zohocorp.com>
2023-03-01 11:39:47 +0530

    Initial Commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/MyLearningPod/MyLearningPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyLearningPod : NSObject
+@end
+@implementation PodsDummy_MyLearningPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyLearningPod_Example/Pods-MyLearningPod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyLearningPod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyLearningPod_Example
+@end
